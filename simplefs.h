4b830419df167b982fd8cfde74ac3fd2b2ada9d3
Ibrahim Korpeoglu <korpe@cs.bilkent.edu.tr>
2019-12-17 18:51:23 +0300

    modified descriptions of functions.

--- simplefs.h
+++ simplefs.h
@@ -6,7 +6,7 @@
 
 int create_vdisk (char *vdiskfilename, int m);
 /* 
-   This function will be used to create a virtual disk (a Linux file)
+   This function will be used to create a virtual disk (as simple Linux file)
    of certain size. The name of the Linux file is vdiskfilename. 
    The parameter m is used to set the size. 
    Size will be 2^m bytes. If success, 0 will returned; if error, -1
@@ -15,9 +15,10 @@ int create_vdisk (char *vdiskfilename, int m);
 
 int sfs_format (char *vdiskname);
 /*
-  This function is will be used to initialize/create (high level format) 
-  the sfs file system on the virtual disk. On disk structures will 
-  be initialized as part of this call. 
+  This function will be used to initialize/create 
+  an sfs file system on the virtual disk (high-level formatting the disk). 
+  On disk file system structures (like superblock, FAT, etc.) will be 
+  initialized as part of this call. 
   If success, 0 will be returned. If error, -1 will be returned.
  */  
 
@@ -26,12 +27,11 @@ int sfs_mount (char *vdiskname);
 /* 
    This function will be used to mount the file system, i.e., to 
    prepare the file system be used. This is a simple operation. 
-   Basically, it will open the regular Linux file and obtain 
-   a file descriptor.  Other operations will use this
-   file descriptor in the library implementation. This virtual 
-   disk (Linux file) descriptor will be a global variable in
-   the library. 
-   If success, 0 will be returned; if error, -1 will be returned. 
+   Basically, it will open the regular Linux file (acting as the virtual disk) 
+   and obtain an integer file descriptor.  Other operations in the library 
+   will use this file descriptor. This descriptor will be a global variable 
+   in the library. If success, 0 will be returned; if error, -1 will 
+   be returned. 
  */ 
 
 int sfs_umount (); 
@@ -45,26 +45,33 @@ int sfs_umount ();
 int sfs_create(char *filename);
 /* 
    With this, an applicaton will create a new file with name 
-   filename. 
-   if success, 0 will be returned. If error, -1 will be returned.  
+   filename. You library implementation of this function will 
+   use an entry in the root directory to store information 
+   about the created file, like its name, size, first data block
+   number, etc. 
+   If success, 0 will be returned. If error, -1 will be returned.  
  */
 
 int sfs_open(char *filename, int mode);
 /*
    With  this an application will open a file. The name of the 
-   to open is filename. The mode paramater specifies if the 
+   file to open is filename. The mode paramater specifies if the 
    file will be opened in read-only mode or in append-only mode. 
-   if 0, read-only; if 1, append-only. We can just 
-   read file or append to it. A file can not be opened for both
-   reading and appending at the same time. 
-   The return value will be a non-negative integer acting as a
+   if 0, read-only; if 1, append-only. We can either  
+   read the file or append to it. A file can not be opened for both
+   reading and appending at the same time. In your library you 
+   should have a open file table and entry in that table will be 
+   used for the opened file. The index of that entry can be returned
+   as the return value of this function. 
+   Hence the return value will be a non-negative integer acting as a
    file descriptor to be used in subsequent file operations. 
    if error, -1 will be returned. 
  */
 
 int sfs_close(int fd);
 /* 
-   With this an application will close a file whose descriptor is fd. 
+   With this an application will close a file whose descriptor is fd.
+   The related open file table entry should be marked as free. 
 */
 
 int sfs_read(int fd, void *buf, int n);
@@ -79,19 +86,20 @@ int sfs_read(int fd, void *buf, int n);
 
 int sfs_append(int fd, void *buf, int n);
 /* 
-   With this, an application can append new data to the file. 
-   fd is the file descriptor. buf is pointing to a static array 
-   holding the data or a dynamically allocted memory space holding
-   the data. n is the size of data to write into the file. 
-   Upon failure, will return -1. Otherwise, the number of bytes
+   With this, an application can append new data to the file. The 
+   parameter fd is the file descriptor. The parameter buf is pointing to (i.e., 
+   is the address of) a static array holding the data or a dynamically 
+   allocted memory space holding the data. The parameter n is the size of 
+   the data to write (append) into the file. Upon failure, will return -1. 
+   Otherwise, the number of bytes
    successfully appended will be returned. 
  */ 
 
 
 int sfs_delete(char *filename);
 /* 
-   With this, an application can delet a file. The name of the
-   file to be deleted is filename. In succesful, 0 will be returned. 
+   With this, an application can delete a file. The name of the
+   file to be deleted is filename. If succesful, 0 will be returned. 
    In case of an error, -1 will be returned. 
 */ 
 
